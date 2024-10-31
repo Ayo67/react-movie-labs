f28896780683b671324f3058a727ba11faa6c4ab
ayoog <20098135@mail.wit.ie>
2024-10-31 14:48:16 +0000

    Added a fix to actor details component

--- movies/src/components/actorDetails/index.js
+++ movies/src/components/actorDetails/index.js
@@ -12,7 +12,21 @@ const ActorDetails = ({ actor }) => {
                 component="img"
                 image={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
                 alt={actor.name}
-                sx={{ height: 400 }} // Adjust height as needed
+                sx={{ height: 400 }}
             />
-        
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {actor.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {actor.biography}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Known For: {actor.known_for.map(film => film.title).join(', ')}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
 export default ActorDetails;
